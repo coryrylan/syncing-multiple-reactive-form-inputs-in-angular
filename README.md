acbe97489af52076966a78dbe135d304dbc4e1e9
coryrylan <splintercode.cb@gmail.com>
2025-06-07 18:37:28 -0500

    init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # syncing-multiple-reactive-form-inputs-in-angular
 
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/coryrylan/syncing-multiple-reactive-form-inputs-in-angular)
+
+https://coryrylan.com/blog/syncing-multiple-reactive-form-inputs-in-angular
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
+  imports: [ReactiveFormsModule],
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  <a href="https://coryrylan.com/blog/syncing-multiple-reactive-form-inputs-in-angular"><h1>Keeping multiple reactive form controls in sync</h1></a>
+  <input placeholder="search 1" [formControl]="search" [value]="search.value" />
+  <input placeholder="search 2" [formControl]="search" [value]="search.value" />
+  <input placeholder="search 3" [formControl]="search" [value]="search.value" />
+`
 })
-export class App {
-  name = 'Angular';
+export class App  {
+  search = new FormControl('hello');
+  subscription = this.search.valueChanges.subscribe(v => console.log(v));
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
+
 
 bootstrapApplication(App);
